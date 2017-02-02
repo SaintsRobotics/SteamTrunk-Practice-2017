af0b9c31673184a18f71105c5af0022b278dff15
s-saxenae <s-saxenae@54S50012399.it.bsd405.org>
2017-02-01 16:01:35 -0800

    Changed DropperTask to RunContinuousTask

--- src/com/saintsrobotics/corebot/tasks/DropperTask.java
+++ src/com/saintsrobotics/corebot/tasks/DropperTask.java
@@ -1,19 +1,20 @@
 package com.saintsrobotics.corebot.tasks;
 
+import com.saintsrobotics.corebot.coroutine.RunContinuousTask;
 import com.saintsrobotics.corebot.coroutine.Task;
 import com.saintsrobotics.corebot.Robot;
 
 //switch to 90 degrees after pressing A
-public class DropperTask extends Task {
-	public void run(){
+public class DropperTask extends RunContinuousTask {
+	public DropperTask() {
 		Robot.servos.gearDropArm.setAngle(45.0);
-		while (true) {
-			wait.until(() -> Robot.oi.drive.buttons.A());
-			Robot.servos.gearDropArm.setAngle(90.0);
-			wait.forSeconds(5.0);
-			Robot.servos.gearDropArm.setAngle(45.0);
-		}
-		
+	}
+	@Override
+	protected void runContinuously() {
+		wait.until(() -> Robot.oi.drive.buttons.A());
+		Robot.servos.gearDropArm.setAngle(90.0);
+		wait.forSeconds(5.0);
+		Robot.servos.gearDropArm.setAngle(45.0);		
 	}
 	
 }
